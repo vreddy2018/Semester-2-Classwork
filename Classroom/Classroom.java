3987085a53216f79d64c08c563a1621342552320
Vineil Reddy <neilreddy@Vineils-MacBook-Pro.local>
2017-02-02 12:32:38 -0500

    Added comments

--- Classroom/Classroom.java
+++ Classroom/Classroom.java
@@ -1,13 +1,29 @@
+/**
+* Authors: Ami, Neil, Dhruv
+* This class generates a classroom and calls upon the Student class
+* The class has a period and a list of students
+*/
+
 public class Classroom
 {
-	
+	/**
+	* Nonstatic class field that is the array of the students in the class
+	*/
 	private Student[] section;
+	
+	/**
+	* Nonstatic class field that is the period of the class as an int
+	*/
 	private int period;
 	
-
+	/**
+	* Default constructor. Takes in no parameters
+	* Creates a default classroom with 13 students and makes
+	* each student the default student
+	*/
 	public Classroom()
 	{
-		section = new Student[13];  //initializing
+		section = new Student[13]; 
 		
 		for (int i = 0; i < section.length; i ++)
 		{
@@ -15,10 +31,14 @@ public class Classroom
 		}	
 		
 		
-		period = 0;  //initializes period
-		//this(new Sudent(), new Student(14, 9));
+		period = 0; 
 	}
 
+	/**
+	* Overloaded constructor. Takes in a list of students and the period int
+	* @param Student[] a - the section/list of students in the class
+	* @param int p - the period of the class
+	*/
 	public Classroom(Student[] a, int p)
 	{
 		section = a;
@@ -27,41 +47,58 @@ public class Classroom
 	
 	
 	
-	//accessors
+	/**
+	* List of students accessor
+	* @return - Student[] section - the list of the students
+	*/
 	public Student[] getSection()
 	{
 		return section;
 	}
 	
+	/**
+	* Specific student - accessor
+	* @param int i - the students position in the class
+	* @return - Student section[i] - the specific student
+	*/
 	public Student getStudent(int i)
 	{
 		return section[i];
 	}	
 	
+	/**
+	* Period accessor
+	* @return - int period - the period of the class
+	*/
 	public int getPeriod()
 	{
 		return period;
 	}
 	
-
 	
-	
-	//modifiers
+	/**
+	* Student modifier - sets one of the students in the class
+	* @param - int i - the position of the student in the section
+	* @param Student s - the student to insert in position i
+	*/
 	public void setStudent(int i, Student s)
 	{
 		section[i] = s;
 	}
 
-
+	/**
+	* Period modifier - changes the period of the class
+	* @param - int i - the period of the class
+	*/
 	public void setPeriod(int i)
 	{
 		period = i;
 	}
 
-
-
-
-	//toString method
+	/**
+	* Returns the period and list of students in the class as a string in readable/printable format
+	* @param String - the period and the list of students in printable format
+	*/
 	public String toString()
 	{
 		String s = "";
@@ -76,7 +113,13 @@ public class Classroom
 	
 	}	
 	
-	
+	/**
+	* This is the nonstatic method to find the oldest student in a classrom
+	* The method works by setting the oldest student as the first one
+	* and then comparing it to the next student using the older method in the student class
+	* if the next student is older, they are set to the new oldest. 
+	* @return oldest - the oldest student
+	*/
 	public Student findOldest()
 	{
 		Student oldest = section[0];
--- Classroom/Student.java
+++ Classroom/Student.java
@@ -1,22 +1,51 @@
+/**
+* Authors: Ami, Neil, Dhruv
+* This class generates a student
+* The student has an age, a name, and their grade
+* There is one default constructor and 2 overloaded constructors
+* In addition to the normal accessors and modifiers, this class
+* has a function that calculates who is older when two students are compared
+*/
+
 public class Student
 {
-	//nonstatic fields
+	/**
+	* Nonstatic class field that is the age of the student as an int
+	*/
 	private int age;
+	
+	/**
+	* Nonstatic class field that is the name of the student as a string
+	*/
 	private String name;
+	
+	/**
+	* Nonstatic class field that is the grade of the student as an int
+	*/
 	private int grade;
 
-	
-	//static fields
+	/**
+	* Static class field that keeps track of how many students are there
+	*/
 	private static int numStudents;
 
-	
-	//constructors
+	/**
+	* Default constructor. Takes in no parameters
+	* Calls the overloaded method, with the default age and grade as 10
+	* and the student's name as "Bob"
+	*/
 	public Student()
 	{
 		this(10,10,"Bob");
 	}
 	
-	
+	/**
+	* Overloaded constructor 1
+	* This constructor takes in the students age and grade
+	* and the default name is Jack
+	* @param int age - the age of the student
+	* @param int grade - the grade of the student
+	*/
 	public Student(int age, int grade)
 	{
 		this.age = age;
@@ -25,6 +54,13 @@ public class Student
 		numStudents++;
 	}
 	
+	/**
+	* Overloaded constructor 2
+	* This constructor takes in the students age, grade and name
+	* @param int a - the age of the student
+	* @param int g - the grade of the student
+	* @param String n - the student's name
+	*/
 	public Student(int a, int g, String n)
 	{
 		age = a;
@@ -33,44 +69,77 @@ public class Student
 		numStudents++;	
 	}
 	
-	//accessors - with these, the above class fields can be made private 
+	
+	
+	/**
+	* Age accessor
+	* @return - int age - the age of the student
+	*/
 	public int getAge()
 	{
 		return age;
 	}
+	
+	/**
+	* Grade accessor
+	* @return - int grade - the grade of the student
+	*/
 	public int getGrade()
 	{
 		return grade;	
 	}
 	
+	/**
+	* Name accessor
+	* @return -  String name - the name of the student
+	*/
 	public String getName()
 	{
 		return name;
 	}
 	
+	/**
+	* Number of students accessor
+	* @return - int numStudents - the number of students
+	*/
 	public static int getNumStudents()
 	{
 		return numStudents;
 	}
 	
 	
-	//modifiers
+	/**
+	* Age modifier - changes the age of a student
+	* @param - int a - the age of the student
+	*/
 	public void setAge(int a)
 	{
 		age = a;
 	}
+	
+	/**
+	* Grade modifier - changes the grade of a student
+	* @param - int g - the grade of the student
+	*/
 	public void setGrade(int g)
 	{
 		grade = g;
 	}
 	
+	/**
+	* Name modifier - changes the name of a student
+	* @param - int n - the name of the student
+	*/
 	public void setName(String n)
 	{
 		name = n;
 	}
 	
 	
-	
+	/**
+	* Returns the name and information of the student as a string
+	* @param String - the name of the student with their age and grade in a readable format
+	*/
 	public String toString()
 	{
 		return name + "(" + age + " years old, " + grade + "th grade) ";
@@ -78,7 +147,15 @@ public class Student
 	}
 	
 	
-	
+	/**
+	* This is the static method to find the elder of two students
+	* The method works by getting the age of both and comparing the two
+	* The method returns the older student
+	* @param Student s1 - the first student
+	* @param Student s2 - the second student
+	* @return Student s1 - the first student is returned if they are older
+	* @return Student s2 - the second student is returned if they are older
+	*/
 	public static Student older(Student s1, Student s2)
 	{
 		if (s1.age>s2.age)
@@ -90,6 +167,14 @@ public class Student
 	
 	}
 	
+	/**
+	* This is the nonstatic method to find the elder of two students
+	* The method works by getting the age of both and comparing the two
+	* The method returns the older student
+	* @param Student s1 - the student the initial student is being compared to
+	* @return Student s1 - the second student is returned if they are older
+	* @return this - the initial student is returned if they are older
+	*/
 	public Student older(Student s1)
 	{
 		if (age>s1.age)
